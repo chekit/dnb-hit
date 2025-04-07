03cfc53f3f0c8e805296ccee7bfff7b858cc71d7
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-04-07 23:07:24 +0200

    add path name to redirect uri

--- scripts/scrits.js
+++ scripts/scrits.js
@@ -86,7 +86,7 @@ function authorizeClient() {
     'user-modify-playback-state',
     'user-read-playback-state',
   ];
-  const redirect_uri = window.location.origin;
+  const redirect_uri = window.location.origin + window.location.pathname;
   const CLIENT_ID = '080aff05f66649f194c9851f0b640de7';
 
   const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${scope.join(
