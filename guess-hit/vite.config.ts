e61dec447f7a50a02e8bca5aaf0ff96c298d8dc0
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-05-30 19:13:45 +0200

    define base url, update build

--- guess-hit/vite.config.ts
+++ guess-hit/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
+  base: './',
   plugins: [svgLoader({
     defaultImport: 'raw'
   })],
