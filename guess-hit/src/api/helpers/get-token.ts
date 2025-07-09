c1a625bd093ec0c365e6857e85185aad4cd3ee37
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-07-09 10:32:04 +0200

    replace old project with vite

--- guess-hit/src/api/helpers/get-token.ts
+++
@@ -1,9 +0,0 @@
-export function getToken() {
-  const { access_token } = history.state;
-
-  if (!access_token) {
-    throw new Error('Access Token not defined!');
-  }
-
-  return access_token;
-}
